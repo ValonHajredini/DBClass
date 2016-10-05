d4ebcb84a3ffc7caf1745af30ae111b0a4a820d8
Arianit Kuçi <arianit.kuci@kujtesa.com>
2016-10-05 17:12:44 +0200

    new version

--- firstProject/index.php
+++ firstProject/index.php
@@ -4,33 +4,28 @@ $db = DB::getIns();
 
 
 echo "<br>====================== The SELECT QUERY ============================<br>";
-//$select = $db->select(
-//    ['users.id','tasks.id','users.firstName', 'users.lastName', 'users.age', 'tasks.taskName'],
-//    ["users", 'inner join' => 'tasks'],
-//    [['users.id','=','tasks.user_id'],['users.firstName','=','valon']],
-//    [10],
-//    ['users.id', 'tasks.id'],
-//    null,
-//    ['users.id' => 'desc', 'tasks.id'=> 'asc']);
+//$select = $db->select(['users.id','tasks.id','users.firstName', 'users.lastName', 'users.age', 'tasks.taskName'],["users", 'inner join' => 'tasks'],[['users.id','=','tasks.user_id'],['users.firstName','=','valon']],[10],['users.id', 'tasks.id'], null,['users.id' => 'desc', 'tasks.id'=> 'asc']);
 //foreach ($select as $key => $rows){
 //    if (is_integer($key)){
 //        echo "<pre>";
 //        print_r($rows);
 //        echo "</pre>";
 //    }
-//
 //}
 echo "OK";
 echo "<br>===================================================================<br>";
 echo "<br>====================== The CREATE QUERY============================<br>";
-
-$db->create('users', ['firstName' => 'Ekoloni','lastName' => 'Ekopro', 'age'=> '111', 'phone' => '+445241521']);
-echo "<br>OK";
-echo"<br>".$db->lastInsertedRow();
+//$db->create(
+//'users',
+// ['firstName' => 'Ekoloni','lastName' => 'Ekopro', 'age'=> '111', 'phone' => '+445241521']
+//);
+echo "OK";
 echo "<br>===================================================================<br>";
 echo "<br>====================== The DELETE QUERY============================<br>";
-
-//$db->delete('users', ['id', '>', 4 ]);
+//$db->delete(
+//'users',
+// ['id', '>', 4 ]
+//);
 echo "OK";
 echo "<br>===================================================================<br>";
 
